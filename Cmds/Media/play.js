8752abca76a3fe82eca4aa466b25ef0b4f87bc5c
keith <157195350+Keithkeizzah@users.noreply.github.com>
2024-10-20 13:40:38 +0300

    𝐊𝐄𝐈𝐓𝐇ヽ( ★ω★)ノᶘ⚈

--- Cmds/Media/play.js
+++ Cmds/Media/play.js
@@ -1,11 +1,10 @@
-
 module.exports = async (context) => {
     const { client, m, text, fetchJson } = context;
 
     const yts = require("yt-search");
 
     try {
-        if (!text) return m.reply("What song do you want to download ?")
+        if (!text) return m.reply("What song do you want to download?");
 
         let search = await yts(text);
         console.log(search); // Log the search results
@@ -18,19 +17,19 @@ module.exports = async (context) => {
         let link = search.all[0].url;
 
         let data = await fetchJson(`https://widipe.com/download/ytdl?url=${link}`);
-        
 
         if (!data || !data.result || !data.result.mp3 || !data.result.title) {
             m.reply("Invalid data.");
             return;
         }
 
+        // Sending the audio in the correct format
         await client.sendMessage(m.chat, {
             audio: { url: data.result.mp3 },
             mimetype: "audio/mp3",
             fileName: `${data.result.title}.mp3`
         }, { quoted: m });
     } catch (error) {
-        m.reply("Download failed\n" + error)
+        m.reply("Download failed\n" + error);
     }
 }
