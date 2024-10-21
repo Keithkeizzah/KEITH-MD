00a793fb21addfddd1fb14c6fd15cb52e4ff3223
keith <157195350+Keithkeizzah@users.noreply.github.com>
2024-10-21 09:49:34 +0300

    𝐊𝐄𝐈𝐓𝐇ヽ( ★ω★)ノᶘ⚈

--- Cmds/AI/design.js
+++ Cmds/AI/design.js
@@ -1,16 +1,27 @@
+const fetch = require('node-fetch'); 
+
 module.exports = async (context) => {
-    const { client, m, text, botname, fetchJson } = context;
+    const { client, m, text, botname } = context;
 
     if (!text) return m.reply("Provide a text");
 
     try {
-        const image = await fetchJson(`https://www.samirxpikachu.run.place/ArcticFL?prompt=${text}`);
         
+        const response = await fetch(`https://www.samirxpikachu.run.place/ArcticFL?prompt=${text}`);
+
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+
+       
+        const arrayBuffer = await response.arrayBuffer();
+        
+        
+        const buffer = Buffer.from(arrayBuffer);
 
         
-
         await client.sendMessage(m.chat, {
-            image: { url: image},
+            image: buffer,
             caption: `Downloaded by ${botname}`
         }, { quoted: m });
 
