7079e109078bb073e794f6838d9d352a16e47c22
keith <157195350+Keithkeizzah@users.noreply.github.com>
2024-10-22 19:36:23 +0300

    𝐊𝐄𝐈𝐓𝐇ヽ( ★ω★)ノᶘ⚈

--- Cmds/Fun/truth.js
+++ Cmds/Fun/truth.js
@@ -1,18 +1,29 @@
-const API_URL = 'https://shizoapi.onrender.com/api/texts/truth';
-const API_KEY = 'shizo';
+const API_URL = 'https://nekos.life/api/v2/fact';
 
 module.exports = async (context) => {
     const { client, m } = context;
 
     try {
-        const response = await fetch(`${API_URL}?apikey=${API_KEY}`);
+        const response = await fetch(`${API_URL}`);
         if (!response.ok) throw new Error('Failed to fetch data');
 
-        const { news } = await response.json();
+        const { data } = await response.json();
+const factMessage = `
+┏━━━━ *ALPHA-FACT* ━━━━━◆                     
+┃
+┃   *◇* ${data.fact} 
+┃
+┃   *◇* Regards *ALPHA MD*
+┃      
+ ╭────────────────◆
+ │ *_Powered by keithkeizzah._*
+ ╰─────────────────◆
+    `;
+    
 
-        await client.sendMessage(m.chat, { text: news }, { quoted: m });
+        await client.sendMessage(m.chat, { text: data }, { quoted: m });
     } catch (error) {
         console.error('Error fetching data:', error);
-        await client.sendMessage(m.chat, { text: 'An error occurred while fetching the news.' }, { quoted: m });
+        await client.sendMessage(m.chat, { text: 'An error occurred while fetching the fact.' }, { quoted: m });
     }
 };
