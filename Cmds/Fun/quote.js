48396f56765b4c32a7fc4ce047db67764e0a33c2
keith <157195350+Keithkeizzah@users.noreply.github.com>
2024-10-22 20:01:51 +0300

    𝐊𝐄𝐈𝐓𝐇ヽ( ★ω★)ノᶘ⚈

--- Cmds/Fun/quote.js
+++ Cmds/Fun/quote.js
@@ -7,20 +7,20 @@ module.exports = async (context) => {
         const response = await fetch(API_URL);
         if (!response.ok) throw new Error('Failed to fetch data');
 
-        const { fact } = await response.json();
+        const { quote } = await response.json();
         const quoteMessage = `
   ┏━━━ *KEITH-FACT* ━━━◆                     
   ┃
-*◇* _${data.quote.body}_
+*◇* _${quote.body}_
   ┃   
-  ┃     *◇* *AUTHOR:* ${data.quote.author}
+  ┃     *◇* *AUTHOR:* ${quote.author}
   ┃      
   ╭──────────────◆
   │ *_Powered by keithkeizzah._*
   ╰──────────────◆
         `;
 
-        await client.sendMessage(m.chat, { text: factMessage }, { quoted: m });
+        await client.sendMessage(m.chat, { text: quoteMessage }, { quoted: m });
     } catch (error) {
         console.error('Error fetching data:', error);
         await client.sendMessage(m.chat, { text: 'An error occurred while fetching the fact.' }, { quoted: m });
