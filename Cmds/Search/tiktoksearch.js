1fb083afb8951788ea52d79da5705f6ebb9b953f
keith <157195350+Keithkeizzah@users.noreply.github.com>
2024-10-24 12:45:31 +0300

    𝐊𝐄𝐈𝐓𝐇ヽ( ★ω★)ノᶘ⚈

--- Cmds/Search/tiktoksearch.js
+++ Cmds/Search/tiktoksearch.js
@@ -1,5 +1,5 @@
 module.exports = async (context) => {
-    const { client, m, text } = context;
+    const { client, m, text, fetchJson } = context;
 
     try {
         if (!text) {
@@ -7,7 +7,7 @@ module.exports = async (context) => {
         }
 
         const query = encodeURIComponent(text);
-        const response = await fetch(`https://apis-starlights-team.koyeb.app/starlight/tiktoksearch?text=${query}`);
+        const response = await fetchJson(`https://apis-starlights-team.koyeb.app/starlight/tiktoksearch?text=${query}`);
 
         if (!response.ok) {
             return m.reply('Error fetching data from API.');
@@ -15,7 +15,6 @@ module.exports = async (context) => {
 
         const results = await response.json();
 
-        // Check if results contain the expected data structure
         if (!results || results.length < 1 || !results[0].nowm) {
             return m.reply('Invalid username or no results found.');
         }
@@ -28,15 +27,16 @@ module.exports = async (context) => {
         const id = item.nowm; 
         const imageUrl = item.imageUrl || ''; 
 
-        const message = `*KEITH-MD TIKTOK SEARCH*\n\nTitle: ${title}\n\nInfo: ${info}\n\nRegion: ${region}\n\nCreator: ${creator}\n\nId: ${id}`;
+        const message = `*KEITH-MD TIKTOK SEARCH*\n\nTitle: ${title}\nInfo: ${info}\nRegion: ${region}\nCreator: ${creator}\nId: ${id}`;
 
+        const options = { quoted: m };
         if (imageUrl) {
-            await client.sendMessage(m.chat, { image: { url: imageUrl }, caption: message }, { quoted: m });
+            await client.sendMessage(m.chat, { image: { url: imageUrl }, caption: message }, options);
         } else {
-            await client.sendMessage(m.chat, { text: message }, { quoted: m });
+            await client.sendMessage(m.chat, { text: message }, options);
         }
     } catch (error) {
-        console.log("Error occurred:", error);
-        return m.reply('An error occurred while processing your request.' + error);
+        console.error("Error occurred:", error);
+        return m.reply('An error occurred while processing your request. Please try again later.');
     }
 };
