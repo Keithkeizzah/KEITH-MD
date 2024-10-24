bf294554ec16402439c82a0a9806910a689d7649
keith <157195350+Keithkeizzah@users.noreply.github.com>
2024-10-24 12:30:47 +0300

    𝐊𝐄𝐈𝐓𝐇ヽ( ★ω★)ノᶘ⚈

--- Cmds/Search/tiktoksearch.js
+++ Cmds/Search/tiktoksearch.js
@@ -37,6 +37,6 @@ module.exports = async (context) => {
         }
     } catch (error) {
         console.log("Error occurred:", error);
-        return m.reply('An error occurred while processing your request.');
+        return m.reply('An error occurred while processing your request.' + error);
     }
 };
