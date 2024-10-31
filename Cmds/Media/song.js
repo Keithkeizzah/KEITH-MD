a2084f3fbabb9f97afa808f75b745633e454e16e
keith <157195350+Keithkeizzah@users.noreply.github.com>
2024-10-31 20:23:40 +0300

    𝐊𝐄𝐈𝐓𝐇ヽ( ★ω★)ノᶘ⚈

---
+++ Cmds/Media/song.js
@@ -0,0 +1,28 @@
+module.exports = async (context) => {
+    const { client, m, text, fetchJson } = context;
+
+const yts = require("yt-search");
+try {
+
+if (!text) return m.reply("What song do you want to download ?")
+
+let search = await yts(text);
+        let link = search.all[0].url;
+
+        let data = await fetchJson (`https://api.dreaded.site/api/ytdl/video?url=${link}`)
+
+
+await client.sendMessage(m.chat, {
+ document: {url: data.result.downloadLink},
+mimetype: "audio/mp3",
+ fileName: `${search.all[0].title}.mp3` }, { quoted: m });
+
+
+} catch (error) {
+
+m.reply("Download failed\n" + error)
+
+}
+
+}
+
