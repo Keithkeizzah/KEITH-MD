25754a68f827b706a452dcdc2be3812225f5f947
keith <157195350+Keithkeizzah@users.noreply.github.com>
2024-11-04 08:50:06 +0300

    𝐊𝐄𝐈𝐓𝐇ヽ( ★ω★)ノᶘ⚈

--- settings.js
+++ settings.js
@@ -21,6 +21,7 @@ const presence = process.env.WA_PRESENCE || 'online';
 const antitag = process.env.ANTITAG || 'true';
 const antidelete = process.env.ANTIDELETE || 'true';
 const autoview = process.env.AUTOVIEW_STATUS || 'true';
+const autolike = process.env.AUTOLIKE_STATUS || 'true';
 const autoread = process.env.AUTOREAD || 'true';
 const autobio = process.env.AUTOBIO || 'false';
 
@@ -33,6 +34,7 @@ module.exports = {
   autobio,
   mode,
   prefix,
+  autolike,
   mycode,
   author,
   packname,
