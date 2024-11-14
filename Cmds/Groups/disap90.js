d278d465cd109af0289f2f38425e3777dd6bec89
keith <157195350+Keithkeizzah@users.noreply.github.com>
2024-11-15 01:06:00 +0300

    Update disap90.js

--- Cmds/Groups/disap90.js
+++ Cmds/Groups/disap90.js
@@ -4,7 +4,7 @@ module.exports = async (context) => {
     await middleware(context, async () => {
         const { client, m } = context;
 
-        await  client.groupToggleEphemeral(m.chat, 7776000);
+        await client.groupToggleEphemeral(m.chat, 7776000);
         m.reply('Disappearing messages successfully turned on for 90 days.');
     });
 };
