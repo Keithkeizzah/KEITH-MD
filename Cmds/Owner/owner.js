f888f439a1693abf905a380da5a3fbb954ec4858
keith <157195350+Keithkeizzah@users.noreply.github.com>
2024-11-15 19:55:57 +0300

    Update owner.js

--- Cmds/Owner/owner.js
+++ Cmds/Owner/owner.js
@@ -1,13 +1,1 @@
-const s = require("../settings.js");  // Importing the settings file
 
-module.exports = async (context) => {
-    const { client, m } = context;
-    const ownernumber = s.dev;  // Get the owner's number from the settings file
-
-    try {
-        await client.sendContact(m.chat, [ownernumber], m);  // Send the contact to the chat
-    } catch (error) {
-        console.error('Error sending owner contact:', error);
-        m.reply('Error sending owner contact.');  // Notify the user if there is an error
-    }
-};
