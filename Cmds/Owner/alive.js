700b4f2141d420d394802330a7a552783f35d483
keith <157195350+Keithkeizzah@users.noreply.github.com>
2024-11-19 19:03:58 +0300

    Create alive.js

---
+++ Cmds/Owner/alive.js
@@ -0,0 +1,50 @@
+module.exports = async (context) => {
+  const { client, m } = context;
+
+  // Sound file URL and other relevant links
+  const name = m.pushName || client.getName(m.sender);
+  const vn = 'https://cdn.jsdelivr.net/gh/Keithkeizzah/KEITH-MD2@main/data/AUD-20241119-WA0011.mp3';
+  const url = 'https://github.com/Keithkeizzah/KEITH-MD2';
+  const murl = 'https://whatsapp.com/channel/0029Vaan9TF9Bb62l8wpoD47';
+  const img = 'https://i.imgur.com/vTs9acV.jpeg';
+
+  // Constructing the contact message
+  const con = {
+    key: {
+      fromMe: false,
+      participant: `${m.sender.split('@')[0]}@s.whatsapp.net`,
+      ...(m.chat ? { remoteJid: '254700143167@s.whatsapp.net' } : {}),
+    },
+    message: {
+      contactMessage: {
+        displayName: name,
+        vcard: `BEGIN:VCARD\nVERSION:3.0\nN:;a,;;;\nFN:${name}\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`,
+      },
+    },
+  };
+
+  // Audio file message with external ad reply info
+  const doc = {
+    audio: {
+      url: vn,
+    },
+    mimetype: 'audio/mpeg',
+    ptt: true,
+    waveform: [100, 0, 100, 0, 100, 0, 100],
+    fileName: 'shizo',
+    contextInfo: {
+      mentionedJid: [m.sender],
+      externalAdReply: {
+        title: 'KEITH MD Alive message',
+        body: 'Regards Keithkeizzah',
+        thumbnailUrl: img,
+        sourceUrl: murl,
+        mediaType: 1,
+        renderLargerThumbnail: true,
+      },
+    },
+  };
+
+  // Send the message
+  await client.sendMessage(m.chat, doc, { quoted: con });
+};
