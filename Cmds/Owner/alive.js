11050297850536c92c48d90453b6d6bc51d5f190
keith <157195350+Keithkeizzah@users.noreply.github.com>
2024-11-30 18:21:49 +0300

    fixed ⚔️🗡️

--- Cmds/Owner/alive.js
+++ Cmds/Owner/alive.js
@@ -1,9 +1,20 @@
 module.exports = async (context) => {
   const { client, m } = context;
 
-  // Sound file URL and other relevant links
+  // Sound file URLs
+  const audioFiles = [
+    'https://cdn.jsdelivr.net/gh/Keithkeizzah/KEITH-MD2@main/data/broken.mp3',
+    'https://cdn.jsdelivr.net/gh/Keithkeizzah/KEITH-MD2@main/data/AUD-20241119-WA0011.mp3',
+    'https://cdn.jsdelivr.net/gh/Keithkeizzah/KEITH-MD2@main/data/home.mp3',
+    'https://cdn.jsdelivr.net/gh/Keithkeizzah/KEITH-MD2@main/data/broken.mp3',
+    'https://cdn.jsdelivr.net/gh/Keithkeizzah/KEITH-MD2@main/data/antidote.m4a'
+  ];
+
+  // Randomly pick an audio file
+  const vn = audioFiles[Math.floor(Math.random() * audioFiles.length)];
+
+  // Other variables
   const name = m.pushName || client.getName(m.sender);
-  const vn = 'https://cdn.jsdelivr.net/gh/Keithkeizzah/KEITH-MD2@main/data/AUD-20241119-WA0011.mp3';
   const url = 'https://github.com/Keithkeizzah/KEITH-MD2';
   const murl = 'https://whatsapp.com/channel/0029Vaan9TF9Bb62l8wpoD47';
   const img = 'https://i.imgur.com/vTs9acV.jpeg';
