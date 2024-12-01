4b141f09a445e1abb3b65663e5bd118d0711ad69
keith <157195350+Keithkeizzah@users.noreply.github.com>
2024-12-01 23:58:56 +0300

    Update vcf.js

--- Cmds/General/vcf.js
+++ Cmds/General/vcf.js
@@ -1,32 +1,38 @@
-       
-
 module.exports = async (context) => {
+    const { client, m, participants, text } = context;
 
- const { client, m, participants, text } = context;
+    // Check if the message is in a group
+    if (!m.isGroup) return m.reply("Command meant for groups");
 
-if (!m.isGroup) return m.reply("Command meant for groups");
+    const fs = require("fs");
+    let gcdata = await client.groupMetadata(m.chat);
+    let gcmem = participants.map(a => a.id);
 
-const fs = require("fs");
-let gcdata = await client.groupMetadata(m.chat)
-let gcmem = participants.map(a => a.id)
+    let vcard = '';
+    let noPort = 0;
 
-let vcard = ''
-let noPort = 0
+    // Iterate over each participant
+    for (let a of gcdata.participants) {
+        let username = a.notify || `⚔️🗡️ALPHA MD${a.id.split("@")[0]}`; // Use username or fallback name
+        vcard += `BEGIN:VCARD\nVERSION:3.0\nFN:${username}\nTEL;type=CELL;type=VOICE;waid=${a.id.split("@")[0]}:+${a.id.split("@")[0]}\nEND:VCARD\n`;
+    }
 
-for (let a of gcdata.participants) {
-    vcard += `BEGIN:VCARD\nVERSION:3.0\nFN:[${noPort++}] +${a.id.split("@")[0]}\nTEL;type=CELL;type=VOICE;waid=${a.id.split("@")[0]}:+${a.id.split("@")[0]}\nEND:VCARD\n`
-}
+    let cont = './contacts.vcf';
 
-let cont = './contacts.vcf'
+    // Notify user that the contact list is being compiled
+    await m.reply('A moment, compiling ' + gcdata.participants.length + ' contacts into a VCF...');
 
-await m.reply('A moment, Keith is compiling '+gcdata.participants.length+' contacts into a vcf...');
+    // Write the VCF data to a file
+    await fs.writeFileSync(cont, vcard.trim());
 
-await fs.writeFileSync(cont, vcard.trim())
+    // Send the VCF file to the group
+    await client.sendMessage(m.chat, {
+        document: fs.readFileSync(cont),
+        mimetype: 'text/vcard',
+        fileName: 'Group contacts.vcf',
+        caption: 'VCF for ' + gcdata.subject + '\n' + gcdata.participants.length + ' contacts'
+    }, { ephemeralExpiration: 86400, quoted: m });
 
-await client.sendMessage(m.chat, {
-    document: fs.readFileSync(cont), mimetype: 'text/vcard', fileName: 'Group contacts.vcf', caption: 'VCF for '+gcdata.subject+'\n'+gcdata.participants.length+' contacts'
-}, {ephemeralExpiration: 86400, quoted: m})
-fs.unlinkSync(cont)
-
-
-}
+    // Clean up by deleting the temporary VCF file
+    fs.unlinkSync(cont);
+};
