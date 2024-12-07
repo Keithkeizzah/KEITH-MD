7b26de9261ac23c58cfcb83f00c444a71f0cac7d
keith <157195350+Keithkeizzah@users.noreply.github.com>
2024-12-07 11:11:08 +0300

    Update blocktags.js

--- Cmds/Owner/blocktags.js
+++ Cmds/Owner/blocktags.js
@@ -22,7 +22,7 @@ module.exports = async (context) => {
                             const phoneNumber = blockedUser.split('@')[0];
 
                             // Add the blocked user's phone number to the message
-                            jackhuh += ` +${phoneNumber}\n`;  // List the phone number
+                            jackhuh += `🗡️  +${phoneNumber}\n`;  // List the phone number
 
                             resolve();
                         }, 500);  // Small delay to avoid overwhelming the client with requests
