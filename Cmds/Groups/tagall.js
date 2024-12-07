3b2c64e35b99088319553b856dc9ab505d961d32
keith <157195350+Keithkeizzah@users.noreply.github.com>
2024-12-07 09:26:55 +0300

    Update tagall.js

--- Cmds/Groups/tagall.js
+++ Cmds/Groups/tagall.js
@@ -4,15 +4,15 @@ module.exports = async (context) => {
     await middleware(context, async () => {
         const { client, m, args, participants, text } = context;
 
+        // Start composing the message
+        let txt = `You have been tagged by ${m.pushName}.\n\nMessage: ${text ? text : 'No Message!'}\n\n`;
 
-let txt = `You have been tagged by ${m.pushName}.
-   
-  Message:- ${text ? text : 'No Message!'}\n\n`; 
-                 for (let mem of participants) { 
-                 txt += `📧 @${mem.id.split('@')[0]}\n`; 
-                 } 
-                 client.sendMessage(m.chat, { text: txt, mentions: participants.map(a => a.id) }, { quoted: m }); 
+        // Loop through the participants and number them
+        participants.forEach((mem, index) => {
+            txt += `${index + 1}. 📧 @${mem.id.split('@')[0]}\n`;
+        });
 
-})
-
-}
+        // Send the message with mentions
+        client.sendMessage(m.chat, { text: txt, mentions: participants.map(a => a.id) }, { quoted: m });
+    });
+};
