df56ab3b797e916547384fbae3c9bfef398f02eb
keith <157195350+Keithkeizzah@users.noreply.github.com>
2024-12-07 10:04:37 +0300

    Update blocklist.js

--- Cmds/Owner/blocklist.js
+++ Cmds/Owner/blocklist.js
@@ -2,16 +2,20 @@ const middleware = require('../../utility/botUtil/Ownermiddleware');
 
 module.exports = async (context) => {
     await middleware(context, async () => {
-        const { client, m, text } = context;
+        const { client, m } = context;
 
-		const blocklist = await message.client.fetchBlocklist();
-		if (blocklist.length > 0) {
-			const mentions = blocklist.map(number => `${number}`);
-			const formattedList = blocklist.map(number => `• @${number.split('@')[0]}`).join('\n');
-			await m.reply(`*_Blocked contacts:_*\n\n${formattedList}`, { mentions });
-		} else {
-			await m.reply('_No blocked Numbers!_');
-		}
-	},
-);
-   
+        try {
+            const blocklist = await client.fetchBlocklist();
+            if (blocklist.length > 0) {
+                const mentions = blocklist.map(number => `${number}`);
+                const formattedList = blocklist.map(number => `• @${number.split('@')[0]}`).join('\n');
+                await m.reply(`*_Blocked contacts:_*\n\n${formattedList}`, { mentions });
+            } else {
+                await m.reply('_No blocked Numbers!_');
+            }
+        } catch (error) {
+            console.error('Error fetching blocklist:', error);
+            await m.reply('_An error occurred while fetching the blocklist._');
+        }
+    });
+};
