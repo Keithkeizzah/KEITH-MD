5410047c3b193479a05f1466a820479be9252c55
keith <157195350+Keithkeizzah@users.noreply.github.com>
2024-12-07 12:59:16 +0300

    Update clear.js

--- Cmds/Owner/clear.js
+++ Cmds/Owner/clear.js
@@ -1,20 +1,10 @@
-const middleware = require('../../utility/botUtil/Ownermiddleware');
+const middleware = require('../../utility/botUtil/middleware');
 
 module.exports = async (context) => {
-    try {
-        // Call the middleware function
-        await middleware(context, async () => {
-            const { client, m } = context;
+    await middleware(context, async () => {
+        const { client, m } = context;
 
-            // Check if clearChat method exists and is valid
-            if (client.clearChat) {
-                await client.clearChat();
-                await m.reply('Chat cleared.');
-            } else {
-                await m.reply('Unable to clear chat. Method not found.');
-            }
-        });
-    } catch (error) {
-        console.error("Error in clearing chat:", error);
-    }
+        await client.clearChat(m.chat, 'not_announcement');
+        m.reply('chats cleared.');
+    });
 };
