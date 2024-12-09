3de1789c79849738009225ad4c99c5759b730017
keith <157195350+Keithkeizzah@users.noreply.github.com>
2024-12-09 21:53:08 +0300

    Update keith.js

--- keith.js
+++ keith.js
@@ -24,7 +24,7 @@ const masterEval = require('./Functions/masterEval');
 
 const {
    presence, autoread, botname,
-   mode, prefix, mycode, author, antilink, packname,
+   mode, prefix, mycode, author, packname,
    dev, gcpresence, antionce, antitag, antibot, antidelete
 } = require('./settings');
 
@@ -91,7 +91,7 @@ module.exports = Keith = async (client, m, chatUpdate, message, store) => {
     const IsGroup = m.chat?.endsWith("@g.us");
 
     const context = {
-        client, m, text, isBotMessage, message, Owner, antilink, chatUpdate, store, isBotAdmin, isAdmin, IsGroup, participants,
+        client, m, text, isBotMessage, message, Owner, chatUpdate, store, isBotAdmin, isAdmin, IsGroup, participants,
         pushname, body, budy, totalCommands, args, mime, qmsg, msgKeith, botNumber, itsMe,
         packname, author, generateProfilePicture, groupMetadata, Keithspeed, mycode,
         fetchJson, exec, getRandom, UploadFileUgu, TelegraPh, prefix, cmd, botname, mode, gcpresence, antibot, antitag, antidelete, antionce, fetchBuffer,store, uploadtoimgur, chatUpdate, ytmp3, getGroupAdmins, Tag
