e9a30c031b726cb8e17fc927c4a5a38968c0cacb
keith <157195350+Keithkeizzah@users.noreply.github.com>
2024-12-09 18:48:04 +0300

    Update index.js

--- index.js
+++ index.js
@@ -10,23 +10,23 @@ const {
   jidDecode,
   proto,
   getContentType,
-} = require('@whiskeysockets/baileys');
-const pino = require('pino');
-const { Boom } = require('@hapi/boom');
-const fs = require('fs');
-const FileType = require('file-type');
-const { exec, spawn, execSync } = require('child_process');
-const axios = require('axios');
-const chalk = require('chalk');
-const figlet = require('figlet');
-const express = require('express');
+} = require("@whiskeysockets/baileys");
+const pino = require("pino");
+const { Boom } = require("@hapi/boom");
+const fs = require("fs");
+const FileType = require("file-type");
+const { exec, spawn, execSync } = require("child_process");
+const axios = require("axios");
+const chalk = require("chalk");
+const figlet = require("figlet");
+const express = require("express");
 const app = express();
 const port = process.env.PORT || 10000;
-const _ = require('lodash');
-const PhoneNumber = require('awesome-phonenumber');
+const _ = require("lodash");
+const PhoneNumber = require("awesome-phonenumber");
 const { imageToWebp, videoToWebp, writeExifImg, writeExifVid } = require('./lib/exif');
 const { isUrl, generateMessageTag, getBuffer, getSizeMedia, fetchJson, await, sleep } = require('./lib/botFunctions');
-const store = makeInMemoryStore({ logger: pino().child({ level: 'silent', stream: 'store' }) });
+const store = makeInMemoryStore({ logger: pino().child({ level: "silent", stream: "store" }) });
 
 const authenticationn = require('./auth.js');
 const { smsg } = require('./smsg');
@@ -35,16 +35,17 @@ const { autoview, autoread, botname, autobio, mode, prefix, autoreact, presence,
 const { DateTime } = require('luxon');
 const { commands, totalCommands } = require('./commandHandler');
 authenticationn();
-const groupEvents = require('./groupEvents.js');
+const groupEvents = require("./groupEvents.js");
 // const connectionEvents = require("./connectionEvents.js");
 
 async function startKeith() {
-  const { saveCreds, state } = await useMultiFileAuthState('session');
+
+  const { saveCreds, state } = await useMultiFileAuthState(`session`)
   const client = KeithConnect({
     logger: pino({ level: 'silent' }),
     printQRInTerminal: true,
     version: [2, 3000, 1015901307],
-    browser: ['KEITH-MD', 'Safari', '3.0'],
+    browser: [`KEITH-MD`, 'Safari', '3.0'],
     fireInitQueries: false,
     shouldSyncHistoryMessage: true,
     downloadHistory: true,
@@ -58,52 +59,57 @@ async function startKeith() {
         const mssg = await store.loadMessage(key.remoteJid, key.id);
         return mssg.message || undefined;
       }
-      return { conversation: 'HERE' };
-    },
+      return { conversation: "HERE" };
+    }
   });
 
   store.bind(client.ev);
+client.ev.on("call", async callData => {
+      if (anticall === 'true') {
+        const callId = callData[0].id;
+        const callerId = callData[0].from;
+        await client.rejectCall(callId, callerId);
+        await client.sendMessage(callerId, {
+          text: "```❗📵I AM KEITH MD | I REJECT THIS CALL BECAUSE MY OWNER IS BUSY.KINDLY SEND TEXT INSTEAD``` ."
+        });
+      }
+    });
+  
+if (autoreact === 'true') {
+  client.ev.on("messages.upsert", async (chatUpdate) => {
+    try {
+      const mek = chatUpdate.messages[0];  // Assuming 'messages' is an array of messages
+      if (!mek || !mek.message) return;
 
-  client.ev.on('call', async (callData) => {
-    if (anticall === 'true') {
-      const callId = callData[0].id;
-      const callerId = callData[0].from;
-      await client.rejectCall(callId, callerId);
-      await client.sendMessage(callerId, {
-        text: '```❗📵I AM KEITH MD | I REJECT THIS CALL BECAUSE MY OWNER IS BUSY. KINDLY SEND TEXT INSTEAD```.',
-      });
-    }
-  });
-   if (autoreact === 'true') {
-    client.ev.on('messages.upsert', async (m) => {
-      const { messages } = m;
       const emojiFilePath = path.resolve(__dirname, 'database', 'emojis.json');
       let emojis = [];
 
       // Ensure emojis file exists and is valid
       try {
         const data = fs.readFileSync(emojiFilePath, 'utf8');
-        emojis = JSON.parse(data); // Parse the JSON data into an array
+        emojis = JSON.parse(data);  // Parse the JSON data into an array
       } catch (error) {
         console.error('Error reading emojis file:', error);
         return;
       }
 
-      // Process each message to react with a random emoji
-      for (const message of messages) {
-        if (!message.key.fromMe && emojis.length > 0) {
-          const randomEmoji = emojis[Math.floor(Math.random() * emojis.length)];
-          await client.sendMessage(message.key.remoteJid, {
-            react: {
-              text: randomEmoji,
-              key: message.key,
-            },
-          });
-        }
+      // Process the message to react with a random emoji
+      if (!mek.key.fromMe && emojis.length > 0) {
+        const randomEmoji = emojis[Math.floor(Math.random() * emojis.length)];
+        await client.sendMessage(mek.key.remoteJid, {
+          react: {
+            text: randomEmoji,
+            key: mek.key,
+          },
+        });
       }
-    });
-  }
-    
+
+    } catch (error) {
+      console.error('Error processing message:', error);
+    }
+  });
+}
+ 
   if (autobio === 'true') {
     setInterval(() => {
       const date = new Date();
@@ -113,18 +119,18 @@ async function startKeith() {
     }, 10 * 1000);
   }
 
-  client.ev.on('messages.upsert', async (chatUpdate) => {
+  client.ev.on("messages.upsert", async (chatUpdate) => {
     try {
-      const mek = chatUpdate.messages[0];
+      mek = chatUpdate.messages[0];
       if (!mek.message) return;
-      mek.message = Object.keys(mek.message)[0] === 'ephemeralMessage' ? mek.message.ephemeralMessage.message : mek.message;
+      mek.message = Object.keys(mek.message)[0] === "ephemeralMessage" ? mek.message.ephemeralMessage.message : mek.message;
 
-      if (autoview === 'true' && autolike === 'true' && mek.key && mek.key.remoteJid === 'status@broadcast') {
+      if (autoview === 'true' && autolike === 'true' && mek.key && mek.key.remoteJid === "status@broadcast") {
         const keithlike = await client.decodeJid(client.user.id);
         await client.sendMessage(mek.key.remoteJid, { react: { key: mek.key, text: '💎' } }, { statusJidList: [mek.key.participant, keithlike] });
       }
 
-      if (autoview === 'true' && mek.key && mek.key.remoteJid === 'status@broadcast') {
+      if (autoview === 'true' && mek.key && mek.key.remoteJid === "status@broadcast") {
         await client.readMessages([mek.key]);
       } else if (autoread === 'true' && mek.key && mek.key.remoteJid.endsWith('@s.whatsapp.net')) {
         await client.readMessages([mek.key]);
@@ -133,20 +139,20 @@ async function startKeith() {
       if (mek.key && mek.key.remoteJid.endsWith('@s.whatsapp.net')) {
         const Chat = mek.key.remoteJid;
         if (presence === 'online') {
-          await client.sendPresenceUpdate('available', Chat);
+          await client.sendPresenceUpdate("available", Chat);
         } else if (presence === 'typing') {
-          await client.sendPresenceUpdate('composing', Chat);
+          await client.sendPresenceUpdate("composing", Chat);
         } else if (presence === 'recording') {
-          await client.sendPresenceUpdate('recording', Chat);
+          await client.sendPresenceUpdate("recording", Chat);
         } else {
-          await client.sendPresenceUpdate('unavailable', Chat);
+          await client.sendPresenceUpdate("unavailable", Chat);
         }
       }
 
-      if (!client.public && !mek.key.fromMe && chatUpdate.type === 'notify') return;
+      if (!client.public && !mek.key.fromMe && chatUpdate.type === "notify") return;
 
-      const m = smsg(client, mek, store);
-      require('./keith')(client, m, chatUpdate, store);
+      m = smsg(client, mek, store);
+      require("./keith")(client, m, chatUpdate, store);
     } catch (err) {
       console.log(err);
     }
@@ -154,88 +160,89 @@ async function startKeith() {
 
   // Handle error
   const unhandledRejections = new Map();
-  process.on('unhandledRejection', (reason, promise) => {
+  process.on("unhandledRejection", (reason, promise) => {
     unhandledRejections.set(promise, reason);
-    console.log('Unhandled Rejection at:', promise, 'reason:', reason);
+    console.log("Unhandled Rejection at:", promise, "reason:", reason);
   });
-  process.on('rejectionHandled', (promise) => {
+  process.on("rejectionHandled", (promise) => {
     unhandledRejections.delete(promise);
   });
-  process.on('uncaughtException', function (err) {
-    console.log('Caught exception: ', err);
+  process.on("Something went wrong", function (err) {
+    console.log("Caught exception: ", err);
   });
 
   // Setting
   client.decodeJid = (jid) => {
     if (!jid) return jid;
     if (/:\d+@/gi.test(jid)) {
-      const decode = jidDecode(jid) || {};
-      return (decode.user && decode.server && decode.user + '@' + decode.server) || jid;
+      let decode = jidDecode(jid) || {};
+      return (decode.user && decode.server && decode.user + "@" + decode.server) || jid;
     } else return jid;
   };
 
   client.getName = (jid, withoutContact = false) => {
-    const id = client.decodeJid(jid);
+    id = client.decodeJid(jid);
     withoutContact = client.withoutContact || withoutContact;
     let v;
-    if (id.endsWith('@g.us'))
+    if (id.endsWith("@g.us"))
       return new Promise(async (resolve) => {
         v = store.contacts[id] || {};
         if (!(v.name || v.subject)) v = client.groupMetadata(id) || {};
-        resolve(v.name || v.subject || PhoneNumber('+' + id.replace('@s.whatsapp.net', '')).getNumber('international'));
+        resolve(v.name || v.subject || PhoneNumber("+" + id.replace("@s.whatsapp.net", "")).getNumber("international"));
       });
     else
       v =
-        id === '0@s.whatsapp.net'
+        id === "0@s.whatsapp.net"
           ? {
               id,
-              name: 'WhatsApp',
+              name: "WhatsApp",
             }
           : id === client.decodeJid(client.user.id)
           ? client.user
           : store.contacts[id] || {};
-    return (withoutContact ? '' : v.name) || v.subject || v.verifiedName || PhoneNumber('+' + jid.replace('@s.whatsapp.net', '')).getNumber('international');
+    return (withoutContact ? "" : v.name) || v.subject || v.verifiedName || PhoneNumber("+" + jid.replace("@s.whatsapp.net", "")).getNumber("international");
   };
 
   client.public = true;
 
   client.serializeM = (m) => smsg(client, m, store);
 
-  client.ev.on('group-participants.update', async (m) => {
+  client.ev.on("group-participants.update", async (m) => {
     groupEvents(client, m);
   });
 
-  client.ev.on('connection.update', async (update) => {
+  client.ev.on("connection.update", async (update) => {
     const { connection, lastDisconnect } = update;
-    if (connection === 'close') {
+    if (connection === "close") {
       let reason = new Boom(lastDisconnect?.error)?.output.statusCode;
       if (reason === DisconnectReason.badSession) {
-        console.log('Bad Session File, Please Delete Session and Scan Again');
+        console.log(`Bad Session File, Please Delete Session and Scan Again`);
         process.exit();
       } else if (reason === DisconnectReason.connectionClosed) {
-        console.log('Connection closed, reconnecting....');
+        console.log("Connection closed, reconnecting....");
         startKeith();
       } else if (reason === DisconnectReason.connectionLost) {
-        console.log('Connection Lost from Server, reconnecting...');
+        console.log("Connection Lost from Server, reconnecting...");
         startKeith();
       } else if (reason === DisconnectReason.connectionReplaced) {
-        console.log('Connection Replaced, Another New Session Opened, Please Restart Bot');
+        console.log("Connection Replaced, Another New Session Opened, Please Restart Bot");
         process.exit();
       } else if (reason === DisconnectReason.loggedOut) {
-        console.log('Device Logged Out, Please Delete File creds.json and Scan Again.');
+        console.log(`Device Logged Out, Please Delete File creds.json and Scan Again.`);
         process.exit();
       } else if (reason === DisconnectReason.restartRequired) {
-        console.log('Restart Required, Restarting...');
+        console.log("Restart Required, Restarting...");
         startKeith();
       } else if (reason === DisconnectReason.timedOut) {
-        console.log('Connection TimedOut, Reconnecting...');
+        console.log("Connection TimedOut, Reconnecting...");
         startKeith();
       } else {
         console.log(`Unknown DisconnectReason: ${reason}|${connection}`);
         startKeith();
       }
-    } else if (connection === 'open') {
-      await client.groupAcceptInvite('KVkQtTxS6JA0Jctdsu5Tj9');
+    } else if (connection === "open") {
+
+      await client.groupAcceptInvite("KVkQtTxS6JA0Jctdsu5Tj9");
 
       console.log(`✅ Connection successful\nLoaded ${totalCommands} commands.\nBot is active.`);
 
@@ -249,67 +256,93 @@ async function startKeith() {
         } else if (currentHour >= 18 && currentHour < 22) {
           return 'Good evening 🌆';
         } else {
-          return 'Good night 😴';
+              return 'Good night 😴';
+            }
+        };
+
+
+        const getCurrentTimeInNairobi = () => {
+            return DateTime.now().setZone('Africa/Nairobi').toLocaleString(DateTime.TIME_SIMPLE);
+        };
+
+        let message = `Holla, ${getGreeting()},\n\n╭═══『𝐊𝐞𝐢𝐭𝐡 𝐌𝐝 𝐢𝐬 𝐜𝐨𝐧𝐧𝐞𝐜𝐭𝐞𝐝』══⊷ \n`;
+
+        message += `║ ʙᴏᴛ ɴᴀᴍᴇ ${botname}\n`;
+message += `║ ᴍᴏᴅᴇ ${mode}\n`;
+message += `║ ᴘʀᴇғɪx [  ${prefix} ]\n`;
+
+message += `║ ᴛᴏᴛᴀʟ ᴘʟᴜɢɪɴs ${totalCommands}\n`
+        message += '║ ᴛɪᴍᴇ ' + getCurrentTimeInNairobi() + '\n';
+        message += '║ ʟɪʙʀᴀʀʏ Baileys\n';
+
+message += `╰═════════════════⊷`
+
+
+
+
+            await client.sendMessage(client.user.id, { text: message });
         }
-      };
 
-      const getCurrentTimeInNairobi = () => {
-        return DateTime.now().setZone('Africa/Nairobi').toLocaleString(DateTime.TIME_SIMPLE);
-      };
 
-      let message = `Holla, ${getGreeting()},\n\n╭═══『𝐊𝐞𝐢𝐭𝐡 𝐌𝐝 𝐢𝐬 𝐜𝐨𝐧𝐧𝐞𝐜𝐭𝐞𝐝』══⊷ \n`;
-      message += `║ ʙᴏᴛ ɴᴀᴍᴇ ${botname}\n`;
-      message += `║ ᴍᴏᴅᴇ ${mode}\n`;
-      message += `║ ᴘʀᴇғɪx [  ${prefix} ]\n`;
-      message += `║ ᴛᴏᴛᴀʟ ᴘʟᴜɢɪɴs ${totalCommands}\n`;
-      message += '║ ᴛɪᴍᴇ ' + getCurrentTimeInNairobi() + '\n';
-      message += '║ ʟɪʙʀᴀʀʏ Baileys\n';
-      message += `╰═════════════════⊷`;
 
-      await client.sendMessage(client.user.id, { text: message });
-    }
+
   });
 
-  client.ev.on('creds.update', saveCreds);
+  client.ev.on("creds.update", saveCreds);
 
-  client.sendText = (jid, text, quoted = '', options) => client.sendMessage(jid, { text: text, ...options }, { quoted });
 
-  client.downloadMediaMessage = async (message) => {
-    let mime = (message.msg || message).mimetype || '';
-    let messageType = message.mtype ? message.mtype.replace(/Message/gi, '') : mime.split('/')[0];
-    const stream = await downloadContentFromMessage(message, messageType);
-    let buffer = Buffer.from([]);
-    for await (const chunk of stream) {
-      buffer = Buffer.concat([buffer, chunk]);
-    }
-    return buffer;
-  };
+  client.sendText = (jid, text, quoted = "", options) => client.sendMessage(jid, { text: text, ...options }, { quoted });
 
-  client.downloadAndSaveMediaMessage = async (message, filename, attachExtension = true) => {
-    let quoted = message.msg ? message.msg : message;
-    let mime = (message.msg || message).mimetype || '';
-    let messageType = message.mtype ? message.mtype.replace(/Message/gi, '') : mime.split('/')[0];
-    const stream = await downloadContentFromMessage(quoted, messageType);
-    let buffer = Buffer.from([]);
-    for await (const chunk of stream) {
-      buffer = Buffer.concat([buffer, chunk]);
-    }
-    let type = await FileType.fromBuffer(buffer);
-    const trueFileName = attachExtension ? filename + '.' + type.ext : filename;
-    await fs.writeFileSync(trueFileName, buffer);
-    return trueFileName;
-  };
+    client.downloadMediaMessage = async (message) => { 
+         let mime = (message.msg || message).mimetype || ''; 
+         let messageType = message.mtype ? message.mtype.replace(/Message/gi, '') : mime.split('/')[0]; 
+         const stream = await downloadContentFromMessage(message, messageType); 
+         let buffer = Buffer.from([]); 
+         for await(const chunk of stream) { 
+             buffer = Buffer.concat([buffer, chunk]) 
+         } 
+
+         return buffer 
+      }; 
+
+
+       
+
+ client.downloadAndSaveMediaMessage = async (message, filename, attachExtension = true) => { 
+         let quoted = message.msg ? message.msg : message; 
+         let mime = (message.msg || message).mimetype || ''; 
+         let messageType = message.mtype ? message.mtype.replace(/Message/gi, '') : mime.split('/')[0]; 
+         const stream = await downloadContentFromMessage(quoted, messageType); 
+         let buffer = Buffer.from([]); 
+         for await(const chunk of stream) { 
+             buffer = Buffer.concat([buffer, chunk]); 
+         } 
+         let type = await FileType.fromBuffer(buffer); 
+         const trueFileName = attachExtension ? (filename + '.' + type.ext) : filename; 
+         // save to file 
+         await fs.writeFileSync(trueFileName, buffer); 
+         return trueFileName; 
+     };
+
 }
+
+
+
+
 
 app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + '/index.html'); 
 });
+
 
 app.listen(port, () => console.log(`Server listening on port http://localhost:${port}`));
 
 startKeith();
+
+
+module.exports = startKeith;
 
 let file = require.resolve(__filename);
 fs.watchFile(file, () => {
