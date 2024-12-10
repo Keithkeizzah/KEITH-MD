8ec060f0aa301604c80f18a41bab5afcc003df81
keith <157195350+Keithkeizzah@users.noreply.github.com>
2024-12-10 06:21:58 +0300

    fixed ⚔️🗡️

--- Cmds/Owner/fullpp.js
+++ Cmds/Owner/fullpp.js
@@ -1,8 +1,9 @@
 const ownerMiddleware = require('../../utility/botUtil/Ownermiddleware'); 
+const { S_WHATSAPP_NET } = require('@whiskeysockets/baileys');
 
 module.exports = async (context) => {
     await ownerMiddleware(context, async () => {
-        const { client, m, text, Owner, generateProfilePicture, botNumber, mime, msgDreaded } = context;
+        const { client, m, text, Owner, generateProfilePicture, botNumber, mime, msgKeith } = context;
 
 try {
 const fs = require("fs");
@@ -11,8 +12,8 @@ if(!msgDreaded) { m.reply('Quote an image...') ; return } ;
 
 
 let media;
-if (msgDreaded.imageMessage) {
-     media = msgDreaded.imageMessage
+if (msgKeith.imageMessage) {
+     media = msgKeith.imageMessage
 
   } else {
     m.reply('This is not an image...'); return
@@ -25,21 +26,29 @@ var medis = await client.downloadAndSaveMediaMessage(media);
                     var {
                         img
                     } = await generateProfilePicture(medis)
-                    await client.query({
-                        tag: 'iq',
-                        attrs: {
-                            to: botNumber,
-                            type: 'set',
-                            xmlns: 'w:profile:picture'
-                        },
-                        content: [{
-                            tag: 'picture',
-                            attrs: {
-                                type: 'image'
-                            },
-                            content: img
-                        }]
-                    })
+
+
+
+
+
+
+client.query({
+                tag: 'iq',
+                attrs: {
+                    target: undefined,
+                    to: S_WHATSAPP_NET,
+                    type:'set',
+                    xmlns: 'w:profile:picture'
+                },
+                content: [
+                    {
+                        tag: 'picture',
+                        attrs: { type: 'image' },
+                        content: img
+                    }
+                ]
+            })
+                    
                     fs.unlinkSync(medis)
                     m.reply("Bot Profile Picture Updated")
 
