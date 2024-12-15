1664eb73357fac9c337f08fb2d3bce0db722fca0
keith <157195350+Keithkeizzah@users.noreply.github.com>
2024-12-15 23:55:01 +0300

    Update sticker.js

--- Cmds/Editting/sticker.js
+++ Cmds/Editting/sticker.js
@@ -1,33 +1,35 @@
 const { Sticker, createSticker, StickerTypes } = require('wa-sticker-formatter');
 
 module.exports = async (context) => {
+  const { client, m, packname, author, msgKeith } = context;
 
-const { client, m, packname, author, msgDreaded } = context;
+  if (!msgKeith) {
+    m.reply('Quote an image or a short video.');
+    return;
+  }
 
-if(!msgDreaded) { m.reply('Quote an image or a short video.') ; return } ;
-let media;
-if (msgDreaded.imageMessage) {
-     media = msgDreaded.imageMessage
-  } else if(msgDreaded.videoMessage) {
-media = msgDreaded.videoMessage
-  } 
- else {
-    m.reply('That is neither an image nor a short video! '); return
-  } ;
+  let media;
+  if (msgKeith.imageMessage) {
+    media = msgKeith.imageMessage;
+  } else if (msgKeith.videoMessage) {
+    media = msgKeith.videoMessage;
+  } else {
+    m.reply('That is neither an image nor a short video!');
+    return;
+  }
 
-var result = await client.downloadAndSaveMediaMessage(media);
+  var result = await client.downloadAndSaveMediaMessage(media);
 
-let stickerResult = new Sticker(result, {
-            pack: packname,
-            author: author,
-            type: StickerTypes.FULL,
-            categories: ["🤩", "🎉"],
-            id: "12345",
-            quality: 70,
-            background: "transparent",
-          });
-const Buffer = await stickerResult.toBuffer();
-          client.sendMessage(m.chat, { sticker: Buffer }, { quoted: m });
+  let stickerResult = new Sticker(result, {
+    pack: packname,
+    author: author,
+    type: StickerTypes.FULL,
+    categories: ["🤩", "🎉"],
+    id: "12345",
+    quality: 70,
+    background: "transparent",
+  });
 
-}
-
+  const Buffer = await stickerResult.toBuffer();
+  client.sendMessage(m.chat, { sticker: Buffer }, { quoted: m });
+};
