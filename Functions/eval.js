f07acfc389a12cc94c7a101fb4931640652769fb
keith <157195350+Keithkeizzah@users.noreply.github.com>
2024-12-15 22:39:05 +0300

    Update eval.js

--- Functions/eval.js
+++ Functions/eval.js
@@ -8,8 +8,8 @@ module.exports = async (client, m, Owner, budy, fetchJson, store) => {
         contextInfo: {
           externalAdReply: {
             showAdAttribution: true,
-            title: "Eval Command Restricted",
-            thumbnailUrl: "https://i.imgur.com/XlQIFIF.jpeg",
+            title: "Only my owner or Keithkeizzah can execute this command 🚫",  // Set the error message as the title
+            thumbnailUrl: "https://i.imgur.com/XlQIFIF.jpeg",  // Thumbnail image
             mediaType: 1,
             sourceUrl: "https://whatsapp.com/channel/0029Vaan9TF9Bb62l8wpoD47",
             renderLargerThumbnail: true,
