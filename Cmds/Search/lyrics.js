3bf56d3a49b7264868dfb15bc494caafb9855ac5
keith <157195350+Keithkeizzah@users.noreply.github.com>
2024-12-25 14:12:09 +0300

    fixed ⚔️🗡️ sync

--- Cmds/Search/lyrics.js
+++ Cmds/Search/lyrics.js
@@ -1,20 +1,45 @@
+const lyricsFinder = require('lyrics-finder');
+const yts = require('yt-search');
+
 module.exports = async (context) => {
+  const { client, m, text } = context;
+  
+  try {
+    
+    if (!text) return m.reply("Please provide a song name and artist.");
+      
+    const info = await yts(text);
+    const results = info.videos;
+  
+    if (!results || results.length === 0) {
+      return m.reply("No results found for the given song or artist.");
+    }
+ 
+    const songDetails = text.split(' ').reverse(); 
+    const title = songDetails.slice(0, songDetails.length - 1).join(' '); 
+    const artist = songDetails[songDetails.length - 1]; 
+        
+    const lyrics = await lyricsFinder(artist, title);
 
-const { client, m, text } = context;
+    if (!lyrics) {
+      return m.reply(`Sorry, I couldn't find any lyrics for "${text}". Please try another song.`);
+    }
 
+    const formattedMessage = `
+*KEITH-MD LYRICS FINDER*
+*Title:* ${title}
+*Artist:* ${artist}
 
-const Genius = require("genius-lyrics");  const Client = new Genius.Client("jKTbbU-6X2B9yWWl-KOm7Mh3_Z6hQsgE4mmvwV3P3Qe7oNa9-hsrLxQV5l5FiAZO"); 
+${lyrics}
+    `;
 
- try { 
- if (!text) return m.reply("Provide a song name!"); 
- const searches = await Client.songs.search(text); 
- const firstSong = searches[0]; 
+    await client.sendMessage(m.chat, { 
+      image: { url: results[0].thumbnail }, 
+      caption: formattedMessage 
+    }, { quoted: m });
 
- const lyrics = await firstSong.lyrics(); 
- await client.sendMessage(m.chat, { text: lyrics}, { quoted: m }); 
- } catch (error) { 
-             m.reply(`I did not find any lyrics for ${text}. Try searching a different song.`); 
-             console.log(error); 
-         } 
-
-}
+  } catch (error) {
+    m.reply(`Error: I was unable to fetch the lyrics. Please try again later.\n\n${error.message}`);
+    console.log(error);
+  }
+};
