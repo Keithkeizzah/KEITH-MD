38ba81f709ead470ccf609338e37a0ddd5c40551
kkeizzah <keizzahketh@gmail.com>
2025-02-09 11:55:19 +0300

    Update vv.js

--- Cmds/General/vv.js
+++ Cmds/General/vv.js
@@ -11,15 +11,16 @@ module.exports = async (context) => {
   try {
     let msg;
     const messageType = Object.keys(m.quoted.message)[0];
+    const quotedMessage = m.quoted.message[messageType];
+    const media = await client.downloadMediaMessage(m.quoted);
 
-    if (['imageMessage', 'videoMessage'].includes(messageType)) {
-      const media = await client.downloadAndSaveMediaMessage(m.quoted);
-      msg = { video: { url: media }, caption: m.quoted.message[messageType].caption || '' };
-    } else if (messageType === 'audioMessage') {
-      const media = await client.downloadAndSaveMediaMessage(m.quoted);
-      msg = { audio: { url: media }, mimetype: 'audio/mp4' };
-    } else if (messageType === 'stickerMessage') {
-      const media = await client.downloadAndSaveMediaMessage(m.quoted);
+    if (/video/.test(messageType)) {
+      msg = { video: media, caption: `Retrieved by Keith\nOriginal caption: ${quotedMessage.caption || ''}` };
+    } else if (/image/.test(messageType)) {
+      msg = { image: media, caption: `Retrieved by Keith\nOriginal caption: ${quotedMessage.caption || ''}` };
+    } else if (/audio/.test(messageType)) {
+      msg = { audio: media, mimetype: 'audio/mp4' };
+    } else if (/sticker/.test(messageType)) {
       const stickerMess = new Sticker(media, {
         pack: 'KEITH-MD',
         type: StickerTypes.CROPPED,
@@ -35,11 +36,10 @@ module.exports = async (context) => {
     }
 
     // Send the message
-    await client.sendMessage(m.chat, msg);
+    await client.sendMessage(m.chat, msg, { quoted: m });
 
   } catch (error) {
     console.error("Error processing the message:", error);
     m.reply('An error occurred while processing your request.');
   }
-};
- 
+}; 
