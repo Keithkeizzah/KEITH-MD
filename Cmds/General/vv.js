5ea49d736f324657fc409365ad1e796e0bf12ef3
kkeizzah <keizzahketh@gmail.com>
2025-02-09 12:30:42 +0300

    Update vv.js

--- Cmds/General/vv.js
+++ Cmds/General/vv.js
@@ -1,35 +1,49 @@
-
 module.exports = async (client, m) => {
-  const textL = m.text.toLowerCase();
+  // Get the quoted message from the context
   const quotedMessage = m.msg?.contextInfo?.quotedMessage;
 
-  if (!quotedMessage || !textL || !m.quoted.chat) {
+  // Check if there is a quoted message
+  if (!quotedMessage) {
     return m.reply("Please quote a status media to save.");
   }
 
   try {
+    // Handle image message
     if (quotedMessage.imageMessage) {
       let imageCaption = quotedMessage.imageMessage.caption || "No caption provided.";
-      let imageUrl = await client.downloadMediaMessage(m.quoted);
-      await client.sendMessage(m.chat, { image: { url: imageUrl }, caption: `Retrieved by Keith\nOriginal caption: ${imageCaption}` }, { quoted: m });
+      let imageUrl = await client.downloadMediaMessage(quotedMessage);
+      await client.sendMessage(m.chat, { 
+        image: { url: imageUrl }, 
+        caption: `Retrieved by Keith\nOriginal caption: ${imageCaption}`
+      }, { quoted: m });
     }
 
+    // Handle video message
     if (quotedMessage.videoMessage) {
       let videoCaption = quotedMessage.videoMessage.caption || "No caption provided.";
-      let videoUrl = await client.downloadMediaMessage(m.quoted);
-      await client.sendMessage(m.chat, { video: { url: videoUrl }, caption: `Retrieved by Keith\nOriginal caption: ${videoCaption}` }, { quoted: m });
+      let videoUrl = await client.downloadMediaMessage(quotedMessage);
+      await client.sendMessage(m.chat, { 
+        video: { url: videoUrl }, 
+        caption: `Retrieved by Keith\nOriginal caption: ${videoCaption}`
+      }, { quoted: m });
     }
 
+    // Handle audio message
     if (quotedMessage.audioMessage) {
-      let audioUrl = await client.downloadMediaMessage(m.quoted);
-      await client.sendMessage(m.chat, { audio: { url: audioUrl }, mimetype: 'audio/mp4' }, { quoted: m });
+      let audioUrl = await client.downloadMediaMessage(quotedMessage);
+      await client.sendMessage(m.chat, { 
+        audio: { url: audioUrl }, 
+        mimetype: 'audio/mp4'
+      }, { quoted: m });
     }
 
+    // Handle sticker message
     if (quotedMessage.stickerMessage) {
-      let stickerUrl = await client.downloadMediaMessage(m.quoted);
-      await client.sendMessage(m.chat, { sticker: stickerUrl }, { quoted: m });
+      let stickerUrl = await client.downloadMediaMessage(quotedMessage);
+      await client.sendMessage(m.chat, { 
+        sticker: stickerUrl
+      }, { quoted: m });
     }
-    
   } catch (error) {
     console.error("Error processing the message:", error);
     m.reply('An error occurred while processing your request.');
