45511f3dcf2b462644fcb17b4a325d5408f2d1e3
keith <157195350+Keithkeizzah@users.noreply.github.com>
2025-02-27 14:00:28 +0300

    Delete Functions directory

--- Functions/gcPresence.js
+++
@@ -1,7 +0,0 @@
-module.exports = async (client, m, gcpresence) => {
-    if (m.isGroup && gcpresence === 'true') {
-        let keithrecordin = ['recording', 'composing'];
-        let keithrecordinfinal = keithrecordin[Math.floor(Math.random() * keithrecordin.length)];
-        await client.sendPresenceUpdate(keithrecordinfinal, m.chat);
-    }
-};
