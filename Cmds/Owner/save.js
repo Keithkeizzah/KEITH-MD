ed24c1a3e4608f1dd24f908726dee51aeeff03bd
keith <157195350+Keithkeizzah@users.noreply.github.com>
2025-02-28 13:37:41 +0300

    Update save.js

--- Cmds/Owner/save.js
+++ Cmds/Owner/save.js
@@ -1,13 +1,23 @@
-/*   Fortunatus :v
-
-What's The Point Of This Code ? */
-
-const ownerMiddleware = require('../../utility/botUtil/Ownermiddleware'); 
-
 module.exports = async (context) => {
-    await ownerMiddleware(context, async () => {
-        const { client, m, Owner } = context;
+  const { client, m } = context;
 
-})
+  const quotedMessage = m.msg?.contextInfo?.quotedMessage;
 
-}
+  if (quotedMessage && !m.quoted.chat.includes("status@broadcast")) {
+    return m.reply("You did not tag a status media to save.");
+  }
+
+  if (quotedMessage && m.quoted.chat.includes("status@broadcast")) {
+    if (quotedMessage.imageMessage) {
+      let imageCaption = quotedMessage.imageMessage.caption;
+      let imageUrl = await client.downloadAndSaveMediaMessage(quotedMessage.imageMessage);
+      client.sendMessage(m.chat, { image: { url: imageUrl }, caption: imageCaption });
+    }
+
+    if (quotedMessage.videoMessage) {
+      let videoCaption = quotedMessage.videoMessage.caption;
+      let videoUrl = await client.downloadAndSaveMediaMessage(quotedMessage.videoMessage);
+      client.sendMessage(m.chat, { video: { url: videoUrl }, caption: videoCaption });
+    }
+  }
+};
