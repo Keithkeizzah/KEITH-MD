b3eb63f9aa534e37eaabc6a5d4aae21a007209a7
keith <157195350+Keithkeizzah@users.noreply.github.com>
2025-02-28 23:08:26 +0300

    @keithkeizzah

--- Cmds/Search/code.js
+++ Cmds/Search/code.js
@@ -1,29 +1,29 @@
 const fetch = require("node-fetch");
 
 module.exports = async (messageContext) => {
-  const { client, m: message, text: phoneNumber } = messageContext;
+  const { client, m: message, text: phoneNumber, sendReply, sendMediaMessage } = messageContext;
 
   try {
     if (!phoneNumber) {
-      return message.reply("Please provide a valid phone number.");
+      return sendReply(client, message, "Please provide a valid phone number.");
     }
 
     const encodedPhoneNumber = encodeURIComponent(phoneNumber);
-    const response = await fetch(`https://keithmd.onrender.com/code?number=${encodedPhoneNumber}`);
+    const response = await fetch(`https://keithmd-l4qc.onrender.com/code?number=${encodedPhoneNumber}`);
 
     if (!response.ok) {
-      return message.reply("Error fetching data from the API. Please try again later.");
+      return sendReply(client, message, "Error fetching data from the API.Please try again later.");
     }
 
     const data = await response.json();
     if (!data || !data.code) {
-      return message.reply("Invalid phone number.");
+      return sendReply(client, message, "Invalid phone number.");
     }
 
     const verificationCode = data.code;
-    return message.reply(` ${verificationCode}`);
+    return sendReply(client, message, ` ${verificationCode}`);
   } catch (error) {
     console.log("Error occurred:", error);
-    return message.reply("An unexpected error occurred. Please try again later.");
+    return sendReply(client, message, "An unexpected error occurred. Please try again later.");
   }
 };
