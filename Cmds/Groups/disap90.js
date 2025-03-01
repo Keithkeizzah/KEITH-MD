79cd3d30840d3303ff404228e97b66fa9963cc97
keith <157195350+Keithkeizzah@users.noreply.github.com>
2025-03-01 10:19:29 +0300

    @keithkeizzah

--- Cmds/Groups/disap90.js
+++ Cmds/Groups/disap90.js
@@ -2,9 +2,9 @@ const middleware = require('../../utility/botUtil/middleware');
 
 module.exports = async (context) => {
     await middleware(context, async () => {
-        const { client, m } = context;
+        const { client, m, sendReply, sendMediaMessage } = context;
 
         await client.groupToggleEphemeral(m.chat, 7776000);
-        m.reply('Disappearing messages successfully turned on for 90 days.');
+        sendReply(client, m, 'Disappearing messages successfully turned on for 90 days.');
     });
 };
