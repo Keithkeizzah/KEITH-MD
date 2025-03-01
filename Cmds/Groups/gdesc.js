ec3993dd651eff2dbbe5a9599628d02bba2f8cd8
keith <157195350+Keithkeizzah@users.noreply.github.com>
2025-03-01 10:31:40 +0300

    @keithkeizzah

--- Cmds/Groups/gdesc.js
+++ Cmds/Groups/gdesc.js
@@ -2,21 +2,21 @@ const middleware = require('../../utility/botUtil/middleware');
 
 module.exports = async (context) => {
     await middleware(context, async () => {
-        const { client, m, text } = context;
+        const { client, m, text, sendReply, sendMediaMessage } = context;
 
-        // Determine the new group description (from text or reply message)
+        
         const description = text || m.reply_message?.text;
         if (!description) {
-            return m.reply('_Please provide a description for the group._');
+            return sendReply(client, m, '_Please provide a description for the group._');
         }
 
         try {
-            // Update the group description using the correct variable
+            
             await client.groupUpdateDescription(m.chat, description);
-            return m.reply('_Group description updated successfully._');
+            return sendReply(client, m, '_Group description updated successfully._');
         } catch (error) {
             console.error('Error updating group description:', error);
-            return m.reply('_Failed to update the group description. Please try again later._');
+            return sendReply(client, m, '_Failed to update the group description. Please try again later._');
         }
     });
 };
