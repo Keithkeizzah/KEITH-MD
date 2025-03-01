4a268b8ba8f7bfd54e36803e2678dc9e008166a1
keith <157195350+Keithkeizzah@users.noreply.github.com>
2025-03-01 09:59:40 +0300

    @keithkeizzah

--- Cmds/Groups/close.js
+++ Cmds/Groups/close.js
@@ -2,9 +2,9 @@ const middleware = require('../../utility/botUtil/middleware');
 
 module.exports = async (context) => {
     await middleware(context, async () => {
-        const { client, m } = context;
+        const { client, m, sendReply, sendMediaMessage } = context;
 
         await client.groupSettingUpdate(m.chat, 'announcement');
-        m.reply('Group closed.');
+        sendReply(client, m, 'Group closed.');
     });
 };
