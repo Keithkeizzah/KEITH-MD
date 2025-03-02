b221c68bbb32e1d755d361f11373189c837fd2c6
keith <157195350+Keithkeizzah@users.noreply.github.com>
2025-03-02 04:20:13 +0300

    @keithkeizzah

--- Cmds/General/sc.js
+++ Cmds/General/sc.js
@@ -1,12 +1,12 @@
 module.exports = async (context) => {
-  const { client, m, botname, author, gurl, url } = context;
+  const { client, m, sendReply, author, botname, sendMediaMessage } = context;
 
   try {
-    // Fetch repository data from GitHub
+    
     const response = await fetch("https://api.github.com/repos/Keithkeizzah/KEITH-MD");
     const repoData = await response.json();
 
-    // Extract relevant information
+    
     const repoInfo = {
       stars: repoData.stargazers_count,
       forks: repoData.forks_count,
@@ -16,11 +16,11 @@ module.exports = async (context) => {
       url: repoData.html_url
     };
 
-    // Format dates
+    
     const createdDate = new Date(repoInfo.createdAt).toLocaleDateString("en-GB");
     const lastUpdateDate = new Date(repoInfo.lastUpdate).toLocaleDateString("en-GB");
 
-    // Construct message caption
+    
     const messageCaption = `
       *Hello ,,,👋 This is ${botname}*
       The best bot in the universe developed by ${author}. Fork and give a star 🌟 to my repo
@@ -31,28 +31,21 @@ module.exports = async (context) => {
       │✞ *Last Update:* ${lastUpdateDate}
       │✞ *Owner:* ${author}
       │✞ *Repository:* ${repoInfo.url}
-      │✞ *Session:* https://keithmd.onrender.com
+      │✞ *Session:* keithmd-l4qc.onrender.com
       ╰───────────────────
     `;
 
-    // Send the generated message to the user
-    await client.sendMessage(m.chat, {
-      text: messageCaption,
-      contextInfo: {
-        mentionedJid: [m.sender], // Mention the sender
-        externalAdReply: {
-          title: botname,
-          body: author,
-          thumbnailUrl: url,
-          sourceUrl: gurl,
-          mediaType: 1,
-          renderLargerThumbnail: true
-        }
-      }
+    
+    const imageUrl = "https://path-to-your-image.jpg"; 
+
+    
+    await sendMediaMessage(client, m, {
+      image: imageUrl,
+      text: messageCaption
     });
 
   } catch (error) {
     console.error("Error:", error);
-    m.reply('An unexpected error occurred while generating the repo information.');
+    await sendReply(client, m, 'An unexpected error occurred while generating the repo information.');
   }
 };
