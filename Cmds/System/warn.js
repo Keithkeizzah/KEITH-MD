ef2136e4b0fb6b3ac9a2914182c3273b41b9a8ae
keith <157195350+Keithkeizzah@users.noreply.github.com>
2025-03-03 13:08:17 +0300

    Create warn.js

---
+++ Cmds/System/warn.js
@@ -0,0 +1,28 @@
+const { addUserWithWarnCount, getWarnCountByJID, resetWarnCountByJID } = require(__dirname + "/../../lib/warn");
+const middleware = require('../../utility/botUtil/middleware');
+
+module.exports = async (context) => {
+  await middleware(context, async () => {
+    const { client, m, text, args } = context;
+    const authorReplied = m.quoted ? m.quoted.participant : m.sender;
+    const warnLimit = 3;
+
+    if (!args.length || args.join('') === '') {
+      await addUserWithWarnCount(authorReplied);
+      let warn = await getWarnCountByJID(authorReplied);
+
+      if (warn >= warnLimit) {
+        await client.sendMessage(m.chat, { text: 'This user has reached the limit of warnings, so they will be kicked out.' }, { quoted: m });
+        await client.groupParticipantsUpdate(m.chat, [authorReplied], "remove");
+      } else {
+        const remaining = warnLimit - warn;
+        await client.sendMessage(m.chat, { text: `This user has been warned. Warnings left before kick: ${remaining}` }, { quoted: m });
+      }
+    } else if (args[0] === 'reset') {
+      await resetWarnCountByJID(authorReplied);
+      m.reply("Warn count has been reset for this user.");
+    } else {
+      m.reply('Reply to a user with ".warn" or ".warn reset" to warn or reset the warn count.');
+    }
+  });
+};
