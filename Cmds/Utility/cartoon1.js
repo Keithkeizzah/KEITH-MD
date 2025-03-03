9027210618379810e40a356b959ac80b114b7e94
keith <157195350+Keithkeizzah@users.noreply.github.com>
2025-03-03 11:19:07 +0300

    @keithkeizzah

--- Cmds/Utility/cartoon1.js
+++ Cmds/Utility/cartoon1.js
@@ -1,4 +1,6 @@
 const Jimp = require('jimp');
+const fs = require('fs');
+const { getRandom } = require(__dirname + "/../../lib/botFunctions");
 
 module.exports = async (context) => {
   const { client, m } = context;
@@ -15,13 +17,19 @@ module.exports = async (context) => {
     const media = await client.downloadAndSaveMediaMessage(quoted);
     const enhancedImagePath = `${media}_enhanced.png`;
 
-    // Enhance the image to a cartoon-like character
+    // Read the image using Jimp
     const image = await Jimp.read(media);
+
+    
     image
-      .resize(1000, Jimp.AUTO)  // Resize to 1000 pixels wide (maintains aspect ratio)
-      .posterize(5)             // Reduce the number of colors
-      .contrast(0.5)            // Increase contrast
-      .write(enhancedImagePath); // Save the enhanced image
+      .resize(1000, Jimp.AUTO)         // Resize to 1000 pixels wide (maintains aspect ratio)
+      .quality(80)                     // Set JPEG quality
+      .posterize(7)                    // Reduce the number of colors
+      .contrast(0.5)                   // Increase contrast
+      .brightness(0.1)                 // Increase brightness
+      .greyscale()                     // Apply a greyscale filter
+      .color([{ apply: 'red', params: [10] }])  // Apply a red tint
+      .write(enhancedImagePath);       // Save the enhanced image
 
     const enhancedImageBuffer = fs.readFileSync(enhancedImagePath);
     await client.sendMessage(m.chat, { image: enhancedImageBuffer, caption: "✨ *Image enhancement to cartoon character successful!*" }, { quoted: m });
