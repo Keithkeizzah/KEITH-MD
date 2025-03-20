609126d6b1139aa8dbab50d0e229fa5c3d96f13e
keith <157195350+Keithkeizzah@users.noreply.github.com>
2025-03-20 22:10:11 +0300

    @keithkeizzah

---
+++ Cmds/Coding/carbon.js
@@ -0,0 +1,34 @@
+module.exports = async (context) => {
+  const { client, m, text, botname } = context;
+
+const fetch = require('node-fetch');
+
+  let cap = `Converted By ${botname}`;
+
+  if (m.quoted && m.quoted.text) {
+    const forq = m.quoted.text;
+
+    try {
+      let response = await fetch('https://carbonara.solopov.dev/api/cook', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          code: forq,
+          backgroundColor: '#1F816D',
+        }),
+      });
+
+      if (!response.ok) return m.reply('API failed to fetch a valid response.')
+
+      let per = await response.buffer();
+
+      await client.sendMessage(m.chat, { image: per, caption: cap }, { quoted: m });
+    } catch (error) {
+      m.reply("An error occured\n" + error)
+    }
+  } else {
+    m.reply('Quote a code message');
+  }
+}
