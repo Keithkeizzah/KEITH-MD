dba3bf8294f8b4be356165e750094793470050dc
kkeizzah <keizzahketh@gmail.com>
2025-03-30 11:53:50 +0300

    Update script.js

--- Cmds/General/script.js
+++ Cmds/General/script.js
@@ -31,7 +31,7 @@ module.exports = async (context) => {
       │✞ *Last Update:* ${lastUpdateDate}
       │✞ *Owner:* ${author}
       │✞ *Repository:* ${repoInfo.url}
-      │✞ *Session:* keithmd-l4qc.onrender.com
+      │✞ *Session:* keith-session.onrender.com
       ╰───────────────────
     `;
 
