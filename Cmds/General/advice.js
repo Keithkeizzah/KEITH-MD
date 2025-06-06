6af010b52087808b88b4c0b31405f2bfde397e12
keith <157195350+Keithkeizzah@users.noreply.github.com>
2025-06-07 00:10:26 +0300

    Delete Cmds directory

--- Cmds/General/advice.js
+++
@@ -1,7 +0,0 @@
-const advice = require("badadvice");
-
-module.exports = async (context) => {
-        const { client, m, sendReply, sendMediaMessage } = context;
-await sendReply(client, m,advice());
-
-}
