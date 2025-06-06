b40d90a503c2b6dc3bf3e1eaea3b3889d473b983
keith <157195350+Keithkeizzah@users.noreply.github.com>
2025-06-07 00:32:59 +0300

    Fixed database alignment 🗿✅

--- settings.js
+++ settings.js
@@ -2,54 +2,70 @@
 
 const session = process.env.SESSION || '';
 
-const prefix = process.env.PREFIX || '.';
-const mycode = process.env.CODE || "254";
+const prefix = process.env.PREFIX || ',';
 const author = process.env.OWNER_NAME || 'Keith';
 const packname = process.env.PACKNAME || 'keith';
 const dev = process.env.OWNER_NUMBER || '254748387615';
 const DevKeith = dev.split(",");
 const botname = process.env.BOTNAME || 'KEITH-MD';
 const mode = process.env.MODE || 'public';
-const gcpresence = process.env.GC_PRESENCE || 'tru';
+const gcpresence = process.env.GC_PRESENCE || 'false';
 const antionce = process.env.ANTIVIEWONCE || 'true';
 const sessionName = "session";
-const presence = process.env.WA_PRESENCE || 'recording';
+const presence = process.env.WA_PRESENCE || 'online';
 const herokuapikey = process.env.HEROKU_API_KEY || '';
 const herokuAppname = process.env.HEROKU_APP_NAME || '';
 const url = process.env.URL || 'https://files.catbox.moe/mikdi0.jpg';
-const gurl = process.env.GURL || 'https://github.com/Keithkeizzah';
-const reactemoji = process.env.EMOJI || '🔗';
+const gurl = process.env.GURL || 'https://whatsapp.com/channel/0029Vaan9TF9Bb62l8wpoD47';
+const reactemoji = process.env.EMOJI || '';
 const antitag = process.env.ANTITAG || 'true';
 const groupControl = process.env.GROUP_CONTROL || 'true';
 const anticall = process.env.ANTICALL || 'true';
 const antidelete = process.env.ANTIDELETE || 'true';
-const antimention = process.env.ANTIMENTION || 'true';
 const antibot = process.env.ANTIBOT || 'true';
-const antilink = process.env.ANTILINK || 'true';
-const antibad = process.env.ANTIBAD || 'true';
 const autoview = process.env.AUTOVIEW_STATUS || 'true';
 const autolike = process.env.AUTOLIKE_STATUS || 'true';
-const chatbot = process.env.CHATBOT || 'false';
+const chatbot = process.env.CHATBOT || 'true';
 const greet = process.env.GREET || 'true';
 const autodownloadstatus = process.env.AUTODOWNLOAD_STATUS || 'false';
 const autostatusreply = process.env.AUTOREPLY_STATUS || 'true';
 const autostatusmsg = process.env.AUTOSTATUS_MSG || 'viewed';
-const greetmsg = process.env.GREET_MSG || 'my owner is unavailable text back later';
+const greetmsg = process.env.GREET_MSG || 'text back later';
 const timezone = process.env.TIMEZONE || 'Africa/Nairobi';
 const autoread = process.env.AUTOREAD || 'true';
 const permit = process.env.PM_PERMIT || 'true';
-const voicechatbot = process.env.VOICECHATBOT || 'trul';
-const voicechatbot2 = process.env.VOICECHATBOT2 || 't';
+const voicechatbot = process.env.VOICECHATBOT || 'true';
+const voicechatbot2 = process.env.VOICECHATBOT2 || 'true';
 const anticallmsg = process.env.ANTICALL_MSG || 'Keith declined your 🤙 call';
-const autobio = process.env.AUTOBIO || 'true';
+const autobio = process.env.AUTOBIO || 'false';
 
+const { Sequelize } = require('sequelize'); // Ensure Sequelize is imported
 
+const DATABASE_URL = process.env.DATABASE_URL || './database.db'; // Define DATABASE_URL properly
+
+const database =
+  DATABASE_URL === './database.db'
+    ? new Sequelize({
+        dialect: 'sqlite',
+        storage: DATABASE_URL,
+        logging: false,
+      })
+    : new Sequelize(DATABASE_URL, {
+        dialect: 'postgres',
+        ssl: true,
+        protocol: 'postgres',
+        dialectOptions: {
+          ssl: { require: true, rejectUnauthorized: false },
+        },
+        logging: false,
+      });
 
 module.exports = {
   sessionName,
   presence,
   autoview,
   autoread,
+  database,
   autodownloadstatus,
   botname,
   voicechatbot,
@@ -65,7 +81,7 @@ module.exports = {
   autostatusmsg,
   autolike,
   anticallmsg,
-  mycode,
+ // mycode,
   chatbot,
   author,
   herokuAppname,
