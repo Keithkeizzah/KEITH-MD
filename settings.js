4b9a47e82e334a6beef9ee2984a5e3002dc592f8
kkeizzah <keizzahketh@gmail.com>
2025-06-12 11:26:36 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -9,38 +9,11 @@ const dev = process.env.OWNER_NUMBER || '254748387615';
 const DevKeith = dev.split(",");
 const botname = process.env.BOTNAME || 'KEITH-MD';
 const mode = process.env.MODE || 'public';
-const gcpresence = process.env.GC_PRESENCE || 'false';
-const antionce = process.env.ANTIVIEWONCE || 'true';
 const sessionName = "session";
-const presence = process.env.WA_PRESENCE || 'online';
-const herokuapikey = process.env.HEROKU_API_KEY || '';
-const herokuAppname = process.env.HEROKU_APP_NAME || '';
 const url = process.env.URL || 'https://files.catbox.moe/mikdi0.jpg';
 const gurl = process.env.GURL || 'https://whatsapp.com/channel/0029Vaan9TF9Bb62l8wpoD47';
-const reactemoji = process.env.EMOJI || '';
-const antitag = process.env.ANTITAG || 'true';
-const groupControl = process.env.GROUP_CONTROL || 'true';
-const anticall = process.env.ANTICALL || 'true';
-const antidelete = process.env.ANTIDELETE || 'true';
-const antibot = process.env.ANTIBOT || 'true';
-const autoview = process.env.AUTOVIEW_STATUS || 'true';
-const autolike = process.env.AUTOLIKE_STATUS || 'true';
-const chatbot = process.env.CHATBOT || 'true';
-const greet = process.env.GREET || 'true';
-const autodownloadstatus = process.env.AUTODOWNLOAD_STATUS || 'false';
-const autostatusreply = process.env.AUTOREPLY_STATUS || 'true';
-const autostatusmsg = process.env.AUTOSTATUS_MSG || 'viewed';
-const greetmsg = process.env.GREET_MSG || 'text back later';
 const timezone = process.env.TIMEZONE || 'Africa/Nairobi';
-const autoread = process.env.AUTOREAD || 'true';
-const permit = process.env.PM_PERMIT || 'true';
-const voicechatbot = process.env.VOICECHATBOT || 'true';
-const voicechatbot2 = process.env.VOICECHATBOT2 || 'true';
-const anticallmsg = process.env.ANTICALL_MSG || 'Keith declined your 🤙 call';
-const autobio = process.env.AUTOBIO || 'false';
-
 const { Sequelize } = require('sequelize'); // Ensure Sequelize is imported
-
 const DATABASE_URL = process.env.DATABASE_URL || './database.db'; // Define DATABASE_URL properly
 
 const database =
@@ -62,41 +35,16 @@ const database =
 
 module.exports = {
   sessionName,
-  presence,
-  autoview,
-  autoread,
-  database,
-  autodownloadstatus,
-  botname,
-  voicechatbot,
-  voicechatbot2,
-  reactemoji,
-  autobio,
-  antilink: groupControl, // Use groupControl for antilink
-  antibad: groupControl, // Use groupControl for antibad
+  database,  
+  botname, 
   mode,
   prefix,
-  anticall,
-  autostatusreply,
-  autostatusmsg,
-  autolike,
-  anticallmsg,
- // mycode,
-  chatbot,
-  author,
-  herokuAppname,
-  herokuapikey,
+  timezone,
+  author,  
   url,
   gurl,
   packname,
   dev,
-  greet,
-  greetmsg,
-  DevKeith,
-  gcpresence,
-  permit,
-  antionce,
+  DevKeith,  
   session,
-  antitag,
-  antidelete,
 };
