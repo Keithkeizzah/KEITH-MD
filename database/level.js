c8aafd66b6e60ad6b461452de5363fd39419e0e8
kkeizzah <keizzahketh@gmail.com>
2025-06-21 22:26:45 +0300

    Update level.js

--- database/level.js
+++ database/level.js
@@ -1,7 +1,7 @@
-const config = require("../set");
+const { database } = require("../settings");
 const { DataTypes } = require('sequelize');
 
-const UsersRankDB = config.DATABASE.define('users_rank', {
+const UsersRankDB = database.define('users_rank', {
     id: {
         type: DataTypes.INTEGER,
         primaryKey: true,
@@ -20,6 +20,10 @@ const UsersRankDB = config.DATABASE.define('users_rank', {
         type: DataTypes.INTEGER,
         defaultValue: 0,
     },
+    level: {  // Added level field
+        type: DataTypes.INTEGER,
+        defaultValue: 1,
+    }
 }, {
     timestamps: false,
 });
@@ -40,16 +44,24 @@ async function addOrUpdateUserData(jid) {
     try {
         const [user, created] = await UsersRankDB.findOrCreate({
             where: { jid },
-            defaults: { xp: 10, messages: 1 }
+            defaults: { xp: 10, messages: 1, level: 1 }
         });
         
         if (!created) {
-            await user.increment({
+            const updatedUser = await user.increment({
                 xp: 10,
                 messages: 1
             });
+            
+            // Check if user should level up (100 XP per level)
+            const xpNeeded = updatedUser.level * 100;
+            if (updatedUser.xp >= xpNeeded) {
+                await updatedUser.increment('level', { by: 1 });
+            }
+            
+            return updatedUser;
         }
-        return true;
+        return user;
     } catch (error) {
         console.error('Error updating user data:', error);
         return false;
@@ -61,16 +73,17 @@ async function getMessagesAndXPByJID(jid) {
     try {
         const user = await UsersRankDB.findOne({ 
             where: { jid },
-            attributes: ['messages', 'xp']
+            attributes: ['messages', 'xp', 'level']
         });
         
         return user ? {
             messages: user.messages,
-            xp: user.xp
-        } : { messages: 0, xp: 0 };
+            xp: user.xp,
+            level: user.level
+        } : { messages: 0, xp: 0, level: 1 };
     } catch (error) {
         console.error('Error getting user stats:', error);
-        return { messages: 0, xp: 0 };
+        return { messages: 0, xp: 0, level: 1 };
     }
 }
 
@@ -80,11 +93,25 @@ async function getTop10Users() {
         return await UsersRankDB.findAll({
             order: [['xp', 'DESC']],
             limit: 10,
-            attributes: ['jid', 'xp', 'messages']
+            attributes: ['jid', 'xp', 'messages', 'level']
         });
     } catch (error) {
         console.error('Error getting top users:', error);
         return [];
+    }
+}
+
+// Get user level
+async function getUserLevel(jid) {
+    try {
+        const user = await UsersRankDB.findOne({
+            where: { jid },
+            attributes: ['level']
+        });
+        return user ? user.level : 1;
+    } catch (error) {
+        console.error('Error getting user level:', error);
+        return 1;
     }
 }
 
@@ -97,6 +124,7 @@ module.exports = {
     addOrUpdateUserData,
     getMessagesAndXPByJID,
     getTop10Users,
+    getUserLevel,
     initUsersRankDB,
     UsersRankDB
 };
