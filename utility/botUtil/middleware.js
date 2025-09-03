a08bb20ee257d746ba88296b63d896072d3611b3
keith <157195350+Keithkeizzah@users.noreply.github.com>
2025-09-03 18:24:34 +0300

    @keithkeizzah

--- utility/botUtil/middleware.js
+++ utility/botUtil/middleware.js
@@ -7,17 +7,17 @@ const middleware = async (context, next) => {
 
   
     if (!m.isGroup) {
-        return sendReply(client, m, "This command is meant for groups");
+        return sendReply(client, m, "*This command is meant for groups*");
     }
 
     
     if (!isAdmin) {
-        return sendReply(client, m, "You need admin privileges to use this command");
+        return sendReply(client, m, "*You need admin privileges to use this command*");
     }
 
     
     if (!isBotAdmin) {
-        return sendReply(client, m, "I need admin privileges to execute this command");
+        return sendReply(client, m, "*I need admin privileges to execute this command*");
     }
 
    
